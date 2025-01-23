6e635e17cac875bc9ee4d2e105e493b1adc54e6d
Sukicodes <51380144+SukainaA@users.noreply.github.com>
2025-01-24 01:46:50 +0300

    basic mechanics

---
+++ scripts/bubble.gd
@@ -0,0 +1,34 @@
+extends RigidBody2D
+
+@export var pop_scale_duration: float = 0.2
+var is_popping: bool = false
+
+var movable :bool = true
+
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(_delta: float) -> void:
+
+	pass
+	
+	
+# for duct logic 
+#func getting_in(pos: Vector2):
+	#var to_duct = pos - global_position
+	#var distance = to_duct.length()
+	#var speed = distance * 10  # Speed increases with distance
+	#var motion = to_duct.normalized() * speed * get_process_delta_time()
+	#move_and_collide(motion)	
+func bubble_pop():
+	is_popping = true
+	var tween = create_tween()
+	tween.tween_property(self, "scale", Vector2.ZERO, pop_scale_duration)
+	tween.tween_callback(queue_free)
+
+
+func _on_body_entered(body: Node) -> void:
+	if ! body.is_in_group("fan"):
+		bubble_pop()
---
+++ scripts/fan.gd
@@ -0,0 +1,37 @@
+extends Area2D
+
+var blow_force: float = 40.0
+#@export var max_blow_distance: float = 120.0
+
+func _process(_delta):
+	global_position = get_global_mouse_position()
+	#$Sprite2D.rotation_degrees+= 10
+	
+	#if Input.is_action_just_pressed("enable_fan"):
+		#enable_fan()
+	#if Input.is_action_just_pressed("disable_fan"):
+		#disable_fan()
+		
+
+func _ready() -> void:
+	#Input.set_mouse_mode(Input.MOUSE_MODE_HIDDEN)
+	enable_fan()
+	
+func _on_body_entered(body):
+	if body.is_in_group("bubble") && body.movable :
+		# please work
+		#calculate direction from fan to bag
+		
+		var direction = body.global_position - global_position
+		#distance between fan and bag
+		#var distance = direction.length()
+		direction = direction.normalized()
+		body.apply_impulse(direction * blow_force)
+
+func enable_fan():
+	#$AnimationPlayer.active = true
+	$CollisionShape2D.disabled = false
+
+func disable_fan():
+	#$AnimationPlayer.active = false
+	$CollisionShape2D.disabled = true
