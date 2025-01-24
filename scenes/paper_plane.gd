771907c28362a670f1df7681f6fe210c6c105ddc
draugfein <67869768+draugfein@users.noreply.github.com>
2025-01-24 22:07:33 +0100

    added playground wallahi

---
+++ scenes/paper_plane.gd
@@ -0,0 +1,13 @@
+extends Node2D
+
+@onready var static_body_2d: RigidBody2D = $StaticBody2D
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	static_body_2d.constant_force.x = randi_range(-400, -300)
+	static_body_2d.constant_force.y = randi_range(-80, -30)
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
---
+++ scenes/playground.gd
@@ -0,0 +1,21 @@
+extends Node2D
+@onready var plane_spawner: Node2D = $PlaneSpawner
+
+@onready var timer: Timer = $Timer
+const PAPER_PLANE = preload("res://scenes/paper_plane.tscn")
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+
+
+func _on_timer_timeout() -> void:
+	var plane = PAPER_PLANE.instantiate()
+	plane.position = plane_spawner.position
+	print(plane.position)
+	plane_spawner.add_child(plane)
+	timer.start(randf_range(1.0, 2.2))
