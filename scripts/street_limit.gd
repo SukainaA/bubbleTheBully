1a79f876fb0b651e6fb441bab181672ce4a77b2d
Sukicodes <51380144+SukainaA@users.noreply.github.com>
2025-01-25 09:42:04 +0300

    stuff

---
+++ scripts/street_limit.gd
@@ -0,0 +1,19 @@
+extends Area2D
+
+@export var camera1: Camera2D
+@export var camera2: Camera2D
+
+
+
+
+
+
+func _on_body_entered(body: Node2D) -> void:
+	if body.is_in_group("bubble"):
+		print("body entered")
+		camera1.enabled = true
+		camera2.enabled = false
+		
+		body.global_position.x = 1275 * 2
+		body.global_position.y = 75
+		
