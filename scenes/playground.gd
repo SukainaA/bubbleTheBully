65d828e1015fb52b273df4b980fde0515245d5c1
draugfein <67869768+draugfein@users.noreply.github.com>
2025-01-25 11:01:30 +0100

    planes despawn

--- scenes/playground.gd
+++ scenes/playground.gd
@@ -19,3 +19,8 @@ func _on_timer_timeout() -> void:
 	print(plane.position)
 	plane_spawner.add_child(plane)
 	timer.start(randf_range(1.0, 2.2))
+
+
+func _on_area_2d_body_entered(body: Node2D) -> void:
+	if body.is_in_group("projectile"):
+		body.queue_free()
